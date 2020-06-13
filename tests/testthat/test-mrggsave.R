8aed72619f3510e9ef58f9a57b91d867eef73e8b
Kyle Baron <kyleb@metrumrg.com>
2020-06-12 19:23:25 -0500

    separate patchwork test

--- tests/testthat/test-mrggsave.R
+++ tests/testthat/test-mrggsave.R
@@ -110,15 +110,4 @@ test_that("use_names not passed issue-29", {
   expect_identical(basename(ans),"issue_29.pdf")
 })
 
-context("patchwork objects")
-test_that("patchwork issue-26", {
-  skip_if_not_installed("patchwork")
-  pat <- pg+pg/(pg+pg)
-  expect_is(pat,"patchwork")
-  out <- mrggsave(pat, Script, stem = "patchwork")
-  expect_equal(basename(out), "patchwork.pdf")
-  x <- mrggsave_prep_object(pat)
-  expect_is(x,"gtable")
-})
-
 
