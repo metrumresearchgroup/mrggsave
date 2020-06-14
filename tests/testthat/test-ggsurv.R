b7e062567d80c719477420e8729ed42cf201f17e
Kyle Baron <kyleb@metrumrg.com>
2020-06-13 22:46:31 -0500

    open up test for survminer

--- tests/testthat/test-ggsurv.R
+++ tests/testthat/test-ggsurv.R
@@ -1,28 +1,22 @@
-# library(testthat)
-# library(mrggsave)
-# library(ggplot2)
-# library(glue)
-#
-# testthat::context("test-ggsurv")
-#
-# library(mrggsave)
-# require("survival")
-#
-# test_that("ggsurvplot", {
-#   skip_if_not(require("survminer"))
-#
-#   fit<- survfit(Surv(time, status) ~ sex, data = lung)
-#
-#   # With a new theme
-#   pp1 <- ggsurvplot(fit, data = lung, ggtheme=theme_bw())
-#   x <- mrggsave(pp1, script = "test-ggsurv.R", dir = tempdir(), tag = "with_theme")
-#   expect_identical(basename(x),"test-ggsurv_with_theme.pdf")
-#
-#   # With the default theme
-#   pp2 <- ggsurvplot(fit, data = lung)
-#   x <- mrggsave(pp2, script = "test-ggsurv.R", dir = tempdir(), tag = "no_theme")
-#   expect_identical(basename(x),"test-ggsurv_no_theme.pdf")
-# })
-#
-#
-#
+library(testthat)
+library(mrggsave)
+
+testthat::context("test-ggsurv")
+
+test_that("ggsurvplot", {
+  skip_if_not(require("survminer"))
+
+  data(lung, package = "survival")
+
+  fit <- survival::survfit(survival::Surv(time, status) ~ sex, data = lung)
+
+  # With a new theme
+  pp1 <- survminer::ggsurvplot(fit, data = lung, ggtheme = ggplot2::theme_bw())
+  x <- mrggsave(pp1, script = "test-ggsurv.R", dir = tempdir(), tag = "with_theme")
+  expect_identical(basename(x),"test-ggsurv_with_theme.pdf")
+
+  # With the default theme
+  pp2 <- survminer::ggsurvplot(fit, data = lung)
+  x <- mrggsave(pp2, script = "test-ggsurv.R", dir = tempdir(), tag = "no_theme")
+  expect_identical(basename(x),"test-ggsurv_no_theme.pdf")
+})
