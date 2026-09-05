38d3f811f12456ef86feead0aec0f0c63dfe28a9
Kyle Baron <kyleb@metrumrg.com>
2026-09-05 17:01:57 -0500

    update modification of args

--- R/convert-dev-args.R
+++ R/convert-dev-args.R
@@ -1,13 +1,12 @@
 convert_to_CairoPDF <- function(args) {
-  within(
-    args, {
-      author <- "mrggsave"
-      subject <-  ""
-      creator <-  ""
-      keywords <-  ""
-      title <-  ""
-      create.date <-  "D:19600101"
-      modify.date <-  "D:19600101"
-    }
+  defaults <- list(
+    author = "mrggsave",
+    subject = "",
+    creator = "",
+    keywords = "",
+    title = "",
+    create.date = "D:19600101",
+    modify.date = "D:19600101"
   )
+  modifyList(defaults, args)
 }
