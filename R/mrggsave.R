3d2ab48841f8f2cab714d2d6969c02918db761b7
Kyle Baron <kyleb@metrumrg.com>
2026-09-10 09:24:59 -0500

    drop doc line about Cairo install

--- R/mrggsave.R
+++ R/mrggsave.R
@@ -129,8 +129,6 @@
 #' - No other back-end arguments to [Cairo::CairoPDF()] are set by mrggsave
 #'   and no other back-end arguments can be manipulated by the user.
 #'
-#' Users will be prompted to install the `Cairo` package if it cannot be found.
-#'
 #' @seealso [mrggdraw()], [mrggsave_list()]
 #'
 #' @md
